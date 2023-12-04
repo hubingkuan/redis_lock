08dc8b4c6e9e2f215e841d566b21df0a3023d76c
light <light@89trillion.com>
2023-12-04 16:13:19 +0800

    1

--- lua.go
+++ lua.go
@@ -64,26 +64,57 @@ else
 end
 `
 
-/*
-
- func (d *DataBases) BatchIncrUserStoreResource(uid string, resource []*config.CommonItem) (err error) {
- 	userStoreKey := userStoreResource + uid
- 	userCurrencyKey := userCurrencyCache + uid
- 	m := make(map[int]int, len(resource))
- 	for _, v := range resource {
- 		m[v.ItemId] = v.ItemNumber
- 	}
- 	bytes, _ := json.Marshal(m)
- 	args := []interface{}{string(bytes)}
- 	reply := d.RDB.Eval(context.Background(), lua.LuaCheckAndUpdateUserStoreResource, []string{userStoreKey, userCurrencyKey}, args)
- 	ok, err := reply.Int()
- 	if ok == 1 {
- 		return err
- 	} else {
- 		return constant.ErrorAddReward
- 	}
- }
+// 复活指令的原子化组装  将多个bit位同时置为1
+const LuaBloomBatchSetBits = `
+  local bloomKey = KEYS[1]
+  local bitsCnt = ARGV[1]
 
 
+  for i=1,bitsCnt,1 do
+    local offset = ARGV[1+i]
+    redis.call('setbit',bloomKey,offset,1)
+  end
+  return 1
+`
 
-*/
+// 对多个bit位进行查询  只要有一个bit位的标识位0 则返回0  如果所有bit位的标识都为1  则返回1
+const LuaBloomBatchGetBits = `
+  local bloomKey = KEYS[1]
+  local bitsCnt = ARGV[1]
+  for i=1,bitsCnt,1 do
+    local offset = ARGV[1+i]
+    local reply = redis.call('getbit',bloomKey,offset)
+    if (not reply) then
+        error('FAIL')
+        return 0
+    end
+    if (reply == 0) then
+        return 0
+    end
+  end
+  return 1
+`
+
+//
+//
+//  func (d *DataBases) BatchIncrUserStoreResource(uid string, resource []*config.CommonItem) (err error) {
+//  	userStoreKey := userStoreResource + uid
+//  	userCurrencyKey := userCurrencyCache + uid
+//  	m := make(map[int]int, len(resource))
+//  	for _, v := range resource {
+//  		m[v.ItemId] = v.ItemNumber
+//  	}
+//  	bytes, _ := json.Marshal(m)
+//  	args := []interface{}{string(bytes)}
+//  	reply := d.RDB.Eval(context.Background(), lua.LuaCheckAndUpdateUserStoreResource, []string{userStoreKey, userCurrencyKey}, args)
+//  	ok, err := reply.Int()
+//  	if ok == 1 {
+//  		return err
+//  	} else {
+//  		return constant.ErrorAddReward
+//  	}
+//  }
+//
+//
+//
+//
